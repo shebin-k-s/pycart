2196dfe0c83f20d65076e91245cd27a59227403e
shebin ks <shebinks01@gmail.com>
2025-06-03 18:37:28 +0530

    data model design

---
+++ orders/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+from customers.models import Customer
+from products.models import Product
+
+
+class Order(models.Model):
+    LIVE = 1
+    DELETE = 0
+    DELETE_CHOICES = ((LIVE, 'Live'), (DELETE, 'Delete'))
+    CART_STAGE = 0
+    ORDER_CONFIRMED = 1
+    ORDER_PROCESSED = 2
+    ORDER_DELIVERED = 3
+    ORDER_REJECTED = 4
+    STATUS_CHOICE = ((CART_STAGE, "CART_STAGE"),
+                     (ORDER_CONFIRMED, "ORDER_CONFIRMED"),
+                     (ORDER_PROCESSED, "ORDER_PROCESSED"),
+                     (ORDER_REJECTED, "ORDER_REJECTED"),
+                     (ORDER_REJECTED, "ORDER_REJECTED"),
+                     )
+
+    order_status = models.IntegerField(
+        choices=STATUS_CHOICE,
+        default=CART_STAGE
+    )
+    owner = models.ForeignKey(
+        Customer, on_delete=models.SET_NULL, null=True, related_name='cart')
+
+    delete_status = models.IntegerField(choices=DELETE_CHOICES, default=LIVE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class OrderedItems(models.Model):
+    product = models.ForeignKey(
+        Product, on_delete=models.SET_NULL, null=True, related_name='added_carts')
+    quantity = models.IntegerField(default=1)
+    owner = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name='added_items')
